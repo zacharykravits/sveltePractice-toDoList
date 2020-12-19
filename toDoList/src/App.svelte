95fef487ea3eaf32e69308528b51018671368ee7
Zachary Allen Kravits <zacharykravits@gmail.com>
2020-12-19 10:53:54 -0500

    quick styling

--- toDoList/src/App.svelte
+++ toDoList/src/App.svelte
@@ -49,8 +49,6 @@
 	};
 
 	const event_deleteTask = (selectedTask) => {
-		console.log(selectedTask);
-
 		let desiredTaskToDelete = tasks.findIndex(
 			(item) => item.id == selectedTask.id
 		);
@@ -61,34 +59,128 @@
 </script>
 
 <style>
+	body,
+	html {
+		margin: 0;
+		padding: 0;
+	}
+
+	p {
+		font-family: Helvetica, sans-serif;
+		padding: 0;
+		margin: 0;
+	}
+
+	button {
+		padding: 8px;
+		border-radius: 4px;
+		border: 1px solid #b4b4b4;
+	}
+
+	.delete-button {
+		color: #646464;
+		background: #ffffff;
+		border: none;
+	}
+
+	.status-button-completed {
+		width: 124px;
+		padding: 8px;
+		background: #93ffd6;
+	}
+
+	.status-button-needs-done {
+		width: 124px;
+		padding: 8px;
+		background: #93ceff;
+	}
+
+	.form {
+		padding: 16px 32px;
+		background: #ffffff;
+		border-bottom: 1px solid #cecece;
+		width: 100%;
+		position: fixed;
+	}
+
+	form > input {
+		padding: 8px;
+		min-width: 320px;
+	}
+
+	form > button {
+		background: #93ceff;
+	}
+
+	.task {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		width: 100%;
+		padding: 8px 0px;
+		border-bottom: 1px solid #cecece;
+	}
+
+	.list {
+		padding: 92px 32px 0px 32px;
+		overflow-y: scroll;
+	}
+
+	.emptyList-message {
+		margin-top: 56px;
+	}
+
+	.task-left {
+		display: flex;
+		align-items: center;
+	}
+
+	.task-left > button {
+		margin-right: 16px;
+	}
 </style>
 
 <main>
 	<form
+		class="form"
 		on:submit={(e) => {
 			event_createTask(e, document.getElementById('input-task-name').value);
 		}}>
 		<input id="input-task-name" type="text" required />
-		<button>Create Task</button>
+		<button>create task</button>
 	</form>
 
-	<div>
+	<div class="list">
 		{#each tasks as task (task.id)}
-			<div>
-				<p>{task.name}</p>
+			<div class="task">
+				<div class="task-left">
+					{#if task.status == false}
+						<button
+							class="status-button-needs-done"
+							on:click={() => {
+								event_changeStatus(task.id);
+							}}>mark complete
+						</button>
+					{:else}
+						<button
+							class="status-button-completed"
+							on:click={() => {
+								event_changeStatus(task.id);
+							}}>completed
+						</button>
+					{/if}
+					<p>{task.name}</p>
+				</div>
+
 				<button
-					on:click={() => {
-						event_changeStatus(task.id);
-					}}>
-					{#if task.status == false}mark complete{:else}completed{/if}
-				</button>
-				<button
+					class="delete-button"
 					on:click={() => {
 						event_deleteTask(task);
-					}}>delete</button>
+					}}>delete
+				</button>
 			</div>
 		{:else}
-			<div>
+			<div class="emptyList-message">
 				<p>You have no tasks. Create some above.</p>
 			</div>
 		{/each}
